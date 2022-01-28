70d9ac4768ab3eef96bea04c6ed9724d4955eee6
John-Salguero <John.Salguero@Revature.net>
2022-01-28 15:12:03 -0800

    Something crazy is happening.

---
+++ ORM/src/test/java/test/ConfigurationFactoryImplementationTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigurationFactoryImplementationTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getConfigurations() {
+    }
+}
