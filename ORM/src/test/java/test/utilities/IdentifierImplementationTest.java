51e447071120cf9866ed938be9f3aa2a7411fa5b
John-Salguero <John.Salguero@Revature.net>
2022-01-30 19:25:56 -0800

    Fixing Errors

---
+++ ORM/src/test/java/test/utilities/IdentifierImplementationTest.java
@@ -0,0 +1,20 @@
+package test.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentifierImplementationTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
