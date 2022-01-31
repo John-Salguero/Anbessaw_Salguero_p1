405a102b4e0c86f66e30570e36efc52383048401
Tomas Anbessaw <tomasanbessaw@gmail.com>
2022-01-30 19:27:03 -0800

    git push

---
+++ ORM/src/test/java/test/SessionFactoryImplementationTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import com.salanb.orm.session.SessionFactory;
+import com.salanb.orm.session.SessionFactoryImplementation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionFactoryImplementationTest {
+
+    @Test
+    void getSession() {
+    }
+
+    @Test
+    void close() {
+    }
+
+    @Test
+    void getFieldMaps() {
+    }
+
+    @Test
+    void getTableMaps() {
+    }
+
+    @Test
+    void getTableTypeMaps() {
+    }
+
+    @Test
+    void getPrimaryKeys() {
+    }
+
+    @Test
+    void getId() {
+    }
+}
