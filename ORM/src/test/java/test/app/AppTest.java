6ee2dd4d6a5faa320d4c42920c1abcf415346db0
Tomas Anbessaw <tomasanbessaw@gmail.com>
2022-02-01 13:52:10 -0800

    junit tests

---
+++ ORM/src/test/java/test/app/AppTest.java
@@ -0,0 +1,26 @@
+package com.salanb.orm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getNewSession() {
+    }
+
+    @Test
+    public void testGetNewSession() {
+
+    }
+
+
+    @Test
+    public void getFileFromResource() {
+    }
+}
